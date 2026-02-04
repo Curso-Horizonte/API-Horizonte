e4bc97a98c8febec926b7d5b11ce30cfb876732d
heloisa-machado <heloisa.machado@germinare.org.br>
2026-02-04 07:44:26 -0300

    add a project

---
+++ src/main/java/org/example/apihorizonte/ApiHorizonteApplication.java
@@ -0,0 +1,13 @@
+package org.example.apihorizonte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHorizonteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiHorizonteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/apihorizonte/ApiHorizonteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.apihorizonte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHorizonteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
