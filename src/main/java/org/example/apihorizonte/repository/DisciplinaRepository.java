1a802c1c503ae0a7ccee0ccfa75b225c70e30377
Gustavo De Longhi <gustavodelonghi@gmail.com>
2026-03-05 13:32:46 -0300

    repository tags

--- src/main/java/org/example/apihorizonte/repository/DisciplinaRepository.java
+++ src/main/java/org/example/apihorizonte/repository/DisciplinaRepository.java
@@ -2,6 +2,8 @@ package org.example.apihorizonte.repository;
 
 import org.example.apihorizonte.model.Disciplina;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
 }
